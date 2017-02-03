3fe423f2ffd313986a36dcb02b63e53dac1ba540
manishk <khade.manish09@gmail.com>
2017-02-03 21:46:08 +0530

    Added tests project

---
+++ JenkinsCiCd.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsCiCd.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var assertVal = false;
+            Assert.IsTrue(assertVal);
+        }
+    }
+}
