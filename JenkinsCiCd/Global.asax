9b0d1c77900e81a9526b7c5d64c01a51f265a410
manishk <khade.manish09@gmail.com>
2017-02-03 13:27:02 +0530

    Add project files.

---
+++ JenkinsCiCd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsCiCd.MvcApplication" Language="C#" %>
---
+++ JenkinsCiCd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsCiCd.Startup))]
+namespace JenkinsCiCd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
